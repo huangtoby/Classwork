dec5ee20cbdbd725778c9b334a26543c335bceef
huangtoby <thuang7616@bths.edu>
2016-09-15 13:53:11 -0400

    Update ProceduralExample.java

--- Period8 Java/src/introduction/ProceduralExample.java
+++ Period8 Java/src/introduction/ProceduralExample.java
@@ -3,7 +3,8 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
     String[] names={"Jillian","Joseph","Jordan"};
-    String[] names={"Freshmen","Junior","Senior"};
+    String[] years={"Freshmen","Junior","Senior"};
+    System.out.println("Hi, my name is"+names[0]+".");
     System.out.println("Hello World");
     System.out.println("Bye World");
   }
