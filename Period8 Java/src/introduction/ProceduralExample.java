5d34b84e0c9a68d53e9666a60e3984c2bc117ba8
Student8 <Student8@10.8.33.175>
2016-09-15 14:01:55 -0400

    Names and years

--- Period8 Java/src/introduction/ProceduralExample.java
+++ Period8 Java/src/introduction/ProceduralExample.java
@@ -4,9 +4,9 @@ public class ProceduralExample {
   public static void main(String[] args){
 	String[] names={"Jillian","Joseph","Jordan"};
     String[] years={"Freshmen","Junior","Senior"};
-    System.out.println("Hi, my name is"+names[0]+"and I am in"+years[0]+".");
-    System.out.println("Hi, my name is"+names[1]+"and I am in"+years[1]+".");
-    System.out.println("Hi, my name is"+names[2]+"and I am in"+years[2]+".");
+    System.out.println("Hi, my name is "+names[0]+" and I am a "+years[0]+".");
+    System.out.println("Hi, my name is "+names[1]+" and I am a "+years[1]+".");
+    System.out.println("Hi, my name is "+names[2]+" and I am a "+years[2]+".");
     System.out.println("Hello World");
     System.out.println("Bye World");
   }
