9b6f3f19c9aa4cc57a7e845c7912cfe4907f533a
Student8 <Student8@10.8.33.175>
2016-09-20 13:04:06 -0400

    0.3

--- Period8 Java/src/introduction/DataTypes.java
+++ Period8 Java/src/introduction/DataTypes.java
@@ -9,6 +9,24 @@ public class DataTypes {
 		System.out.println(3+5.0/2+5*2);
 		System.out.println(3.0+5/2+5*2);
 		System.out.println((int)(3.0+5)/(2+5*2));
+		
+		double d1 = 4.64; 
+		double d2 = 2.0;
+		double d3 = 2.64;
+		System.out.println("d1 : " + d1); 
+		System.out.println("d2 : " + d2); 
+		System.out.println("d3 : " + d3);  
+		System.out.println("d1 - d2 : " + (d1 - d2));
+		
+		String ten="10";
+		String two="2";
+		if(ten.compareTo(two)>0) System.out.println("10 is greater than two");
+		else System.out.print("10 is not greater than two");
+		
+		String s = ""+1;
+		System.out.println(s);
+		//variable outside quotes for its value
+		
 	}
 
 }
