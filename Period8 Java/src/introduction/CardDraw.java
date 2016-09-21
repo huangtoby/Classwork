4ffb278c8041dd4353e5ea61a74df1b8dc86369a
Toby <thuang7616@bths.edu>
2016-09-21 18:55:24 -0400

    0.4

--- Period8 Java/src/introduction/CardDraw.java
+++ Period8 Java/src/introduction/CardDraw.java
@@ -3,11 +3,11 @@ package introduction;
 public class CardDraw {
 
 	public static void main(String[] args) {
-		String[] values = {"The King","The Queen","The Jack"};
+		String[] faces = {"The King","The Queen","The Jack"};
 		String[] suits = {"Spades","Hearts","Clubs","Diamonds"};
-		int value = (int)(Math.random()*values.length);
+		int face = (int)(Math.random()*faces.length);
 		int suit = (int)(Math.random()*suits.length);
-		System.out.println(values[value]+" of "+suits[suit]);
+		System.out.println(faces[face]+" of "+suits[suit]);
 
 	}
 
