d14e0f2ce58025f73fe7586ed7978d4c76f91b2b
Student8 <Student8@10.8.33.175>
2016-09-22 13:19:44 -0400

    1.0

--- Period8 Java/src/introduction/StringPractice.java
+++ Period8 Java/src/introduction/StringPractice.java
@@ -1,8 +1,46 @@
 package introduction;
 
-public class StringPractice {
+import java.util.Scanner;
 
+public class StringPractice {
+	
+	static Scanner input;
+	static String user;
+	
 	public static void main(String[] args) {
+		 //demostrateStringMethods();
+		 createFields();
+		 promptName();
+		 promptForever();
+	}	
+	
+	public static void promptName() {
+		print("Enter your name");
+		user = input.nextLine();
+		print("Glad to meet you, "+user+". "
+			+ "For the rest of time, I will call you "+user+"."
+			+ " You may call me Computer."
+			+ " We should become friends.");
+	}
+
+	public static void promptForever() {
+		while(true){
+			promptInput();
+		}	
+	}
+
+	private static void promptInput() {
+		print("Please type something, "+user+".");
+		String userInput = input.nextLine();
+		print("Congratulations! You typed: "+userInput);
+	}
+	
+	private static void createFields() {
+		input = new Scanner (System.in);
+		user = "";
+	}
+	
+	public static void demostrateStringMethods(){
 		String text1 = new String("Hello World");
 		String text2 = "Hello World";//same as above
 		
@@ -18,11 +56,20 @@ public class StringPractice {
 		if(word1.compareTo(word2) <0){
 			print("word1 comes before word2");
 		}
-		
 	}
 	
-	public static void print(String s){
-		System.out.println(s);
+	public static void print(String s) {
+		String printString = s;
+		int cutoff = 45;
+		if(printString.length() > cutoff){
+			int cut = 1;
+			int cutIndex = cut * cutoff;
+			String currentCut = s.substring(0,cutIndex);
+			
+			
+			printString = currentCut;
+		}
+		System.out.println(printString);
 	}
 }
 
