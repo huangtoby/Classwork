7c3f1eb3147a1dad336980239a78060518afd36d
Student8 <Student8@10.8.33.175>
2016-09-23 13:28:10 -0400

    Due 9/26

---
+++ Period8 Java/src/introduction/ExtraCredit.java
@@ -0,0 +1,92 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class ExtraCredit {
+	
+	static Scanner input;
+	static String user;
+	
+	public static void main(String[] args) {
+		 //demostrateStringMethods();
+		 createFields();
+		 promptName();
+		 promptForever();
+	}	
+	
+	public static void promptName() {
+		print("Enter your name");
+		user = input.nextLine();
+		print("Glad to meet you, "+user+". "
+			+ "For the rest of time, I will call you "+user+"."
+			+ " You may call me Computer."
+			+ " We should become friends.");
+	}
+
+	public static void promptForever() {
+		while(true){
+			promptInput();
+		}	
+	}
+
+	private static void promptInput() {
+		print("Please type something, "+user+".");
+		String userInput = input.nextLine();
+		print("Congratulations! You typed: "+userInput);
+	}
+	
+	private static void createFields() {
+		input = new Scanner (System.in);
+		user = "";
+	}
+	
+	public static void demostrateStringMethods(){
+		String text1 = new String("Hello World");
+		String text2 = "Hello World";//same as above
+		
+		if (text1.equals(text2)){
+			print("These strings are equal:");
+		}
+		print(text1);
+		print(text2);
+		
+		String word1 = "Aardvark";
+		String word2 = "Zyzzyva";
+		
+		if(word1.compareTo(word2) <0){
+			print("word1 comes before word2");
+		}
+	}
+	
+	public static void print(String s) {
+		String printString = s;
+		int cutoff = 10;
+		if(printString.length() > cutoff){
+			for(int i = 0; i*cutoff <s.length(); i++){
+				//append '+='
+				printString += getCut(s, cutoff, i+1)+"\n";
+			}
+		}
+		System.out.println(printString);
+	}
+	
+	private static String getCut(String s, int cutoff, int cut){
+		int cutIndex = cut * cutoff;
+		if(cutIndex >s.length()) cutIndex = s.length();
+		String currentCut = s.substring(0,cutIndex);
+		
+		int indexOfLastSpace = currentCut.length()-1;
+		//start at last index, go backwards
+		for (int i = currentCut.length()-1; i>=0; i--){
+			String letter = currentCut.substring(i, i+1);
+			if(letter.equals(" ")){
+				indexOfLastSpace = i;
+				break;
+			}
+		}
+		//shorten the cut to end on a space
+		currentCut = currentCut.substring(0,indexOfLastSpace);
+		return currentCut;
+	}
+}
+
