119b13887e3e2d7358f480120c949af0a50c6ab5
Student8 <Student8@10.8.33.175>
2016-09-27 14:04:11 -0400

    Chatbot

--- Period8 Java/src/chatbot/TobyMain.java
+++ Period8 Java/src/chatbot/TobyMain.java
@@ -56,8 +56,12 @@ public class TobyMain {
 		//make lowercase
 		searchString = searchString.toLowerCase(); 
 		keyword = keyword.toLowerCase();
+		print("The phrase is "+searchString);
+		print("The keyword is "+keyword);
 		//find first position of keyword
-		int psn = searchString.indexOf(0);
+		int psn = searchString.indexOf(keyword);
+		System.out.println("The keyword was found at "+psn);
+		
 		//keep searching until context keyword is found
 		while(psn >=0){
 			//assume preceded and followed by space
@@ -66,21 +70,57 @@ public class TobyMain {
 			//check character in front, if it exists
 			if(psn >0){
 				before = searchString.substring(psn-1, psn);
+				print("The character before is "+before);
 			}
 			//check if there is a character after the keyword
 			if(psn+ keyword.length() <searchString.length()){
 				after = searchString.substring
 						(psn + keyword.length(), psn + keyword.length()+1);
+				print("The character after is "+after);
 			}
-			if(before.compareTo("a")< 0 && after.compareTo("a") <0){
+			if(before.compareTo("a")< 0 && after.compareTo("a") <0 && noNegations(searchString,psn)){
+				print("Found "+keyword+" at "+psn);
 				return psn;
 			}else{
 				//psn+1 is one space after our current psn, so this finds the NEXT word
 				psn = searchString.indexOf(keyword,psn+1);
+				print("Did not find "+keyword+", checking position " +psn);
 			}
 		}
 		
 		return -1;	
+	}
+	
+	/**This is a helper method. A helper method is a method designed for "helping" a larger 
+	 *method. Because of this, helper methods are generally private because they are only 
+	 *used by only used by the methods they are helping. Also, when you do your project, I
+	 *expect to see helper methods because they are also make code more READABLE
+	 *@param searchString (always lowercase)
+	 *@param psn
+	 *@return "true" if there is no negation words in front of psn
+	*/
+	private static boolean noNegations(String searchString, int psn) {
+		//check to see if the word "no" is in front of psn
+		//check to see if there are 3 spaces in front
+		//check to see if "no" is there
+		if(psn -3 >=0 && searchString.substring(psn-3,psn).equals("no ")){
+			return false;
+		}
+		
+		if(psn -4 >=0 && searchString.substring(psn-4,psn).equals("not ")){
+			return false;
+			
+		}
+		
+		if(psn -6 >=0 && searchString.substring(psn-6,psn).equals("never ")){
+			return false;
+		}	
+		
+		if(psn -4 >=0 && searchString.substring(psn-4,psn).equals("n't ")){
+			return false;
+		}
+			
+		return true;
 	}
 
 	public static String promptInput() {
