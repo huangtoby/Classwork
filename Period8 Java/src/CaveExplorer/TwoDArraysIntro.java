09eb512d76c14e866724421bcdcb69c074584fb2
Student8 <Student8@10.8.33.175>
2016-10-28 13:36:09 -0400

    123

---
+++ Period8 Java/src/CaveExplorer/TwoDArraysIntro.java
@@ -0,0 +1,12 @@
+package CaveExplorer;
+
+import java.util.Arrays;
+
+public class TwoDArraysIntro {
+
+	public static void main(String[] args) {
+		String[] xox = {"x","o","x","o","x"};
+		System.out.println(Arrays.toString(xox));
+	}
+
+}
