0be2c93767671d4eb333e65f328b534be5d9257d
Student8 <Student8@10.8.33.175>
2016-11-28 14:04:28 -0500

    1

---
+++ Period8 Java/src/sort/Pokemon.java
@@ -0,0 +1,58 @@
+package sort;
+
+public class Pokemon {
+	private int level;
+	private int hp;
+	private String name;
+	private boolean poisoned;
+	
+	
+	public Pokemon(String name, int level){
+		hp = 100;
+		this.name = name;
+		this.level = level;
+		this.poisoned = false;
+	}
+	
+	public static void main(String[] args){
+		Pokemon chicken = new Pokemon("chicken",26);
+		Pokemon dog = new Pokemon("dog",26);
+		chicken.iChooseYou;
+		dog.iChooseYou;
+		
+	}
+	
+	public void attack(Pokemon target, Attack attack){
+		if(Math.random() <.9){
+			attack.attack(target);
+			System.out.println("The attack hit");
+		}else{
+			System.out.println("The attack missed");
+		}
+	}
+	
+	public void iChooseYou(){
+		System.out.println(name+", "+name+"!");
+	}
+	
+	public int getHP(){
+		return hp;	
+	}
+	
+	public String getName(){
+		return name;	
+	}
+	
+	public void setHP(int newHP){
+		hp = newHP;	
+	}
+	
+	public void setPoisoned(boolean b){
+		poisoned = b;
+	}
+	
+	public void lapse(){
+		if(poisoned)hp -= 15;
+	}
+
+}
