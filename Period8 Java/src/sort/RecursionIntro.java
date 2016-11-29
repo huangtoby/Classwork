e24e2af9f1ce8def45968e4965734b5f01f48aa7
Student8 <Student8@10.8.33.175>
2016-11-29 14:02:28 -0500

    4123

--- Period8 Java/src/sort/RecursionIntro.java
+++ Period8 Java/src/sort/RecursionIntro.java
@@ -3,20 +3,21 @@ package sort;
 public class RecursionIntro {
 
 	public static void main(String[] args) {
-		System.out.println("Using the for loop:");
-		for(int i = 0; i <5; i++){
-			System.out.println("Hello World! x"+i);
-		}
-		System.out.println("Without using a for loop:");
-		forLoop(5, new Action() {
-			
-			private int value = 0;
-			
-			public void act() {
-				System.out.println("Hello World! x"+value);
-				value++;
-			}
-		});
+		hanoiSolution(3, "A", "B", "C");
+//		System.out.println("Using the for loop:");
+//		for(int i = 0; i <5; i++){
+//			System.out.println("Hello World! x"+i);
+//		}
+//		System.out.println("Without using a for loop:");
+//		forLoop(5, new Action() {
+//			
+//			private int value = 0;
+//			
+//			public void act() {
+//				System.out.println("Hello World! x"+value);
+//				value++;
+//			}
+//		});
 	}
 
 	private static void forLoop(int i, Action action) {
@@ -31,4 +32,31 @@ public class RecursionIntro {
 		}
 	}
 
+//	public static int factorial(int x){
+//		//for loop method
+//		factorial = 1
+//		for(int i = 1; i <1;i++){
+//			factorial = factorial*1;
+//		}
+//		System.out.println(factorial);
+//		return factorial;
+//		
+//		
+//		//recursion method
+//		if(x >1){
+//			return x *factorial(x-1);
+//		}
+//		return 1;
+//		//return 0;
+//	}
+	
+	public static void hanoiSolution(int numberOfDiscs, String startPeg, String midPeg, String endPeg){
+		if(numberOfDiscs <= 1){
+			System.out.println("Move "+startPeg+" to "+endPeg);
+		}else{
+			hanoiSolution(numberOfDiscs-1, startPeg, endPeg, midPeg);
+			hanoiSolution(1, startPeg, midPeg, endPeg);
+			hanoiSolution(numberOfDiscs-1, midPeg, startPeg, endPeg);
+		}
+	}
 }
