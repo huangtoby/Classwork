cd3d4f5ce19c2f4c7be16c0c0ff6e6a8872ae57f
Student8 <Student8@10.8.33.175>
2016-12-08 14:04:00 -0500

    a

--- Period8 Java/src/guiPractice/GUIApplication.java
+++ Period8 Java/src/guiPractice/GUIApplication.java
@@ -16,11 +16,13 @@ public class GUIApplication extends JFrame{
 	public GUIApplication() {
 		//terminate program when window is closed
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setUndecorated(false);
 		int x = 40;
 		int y = 40;
 		int width = 600;
 		int height = 400;
 		setBounds(x, y, width, height);
+		initScreen();
 		setVisible(true);
 	}
 	
