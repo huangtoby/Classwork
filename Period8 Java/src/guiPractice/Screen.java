88bdcbf0515fe15939d113fadfb8f2fed2749216
Student8 <Student8@10.8.33.175>
2016-12-09 13:24:52 -0500

    1

--- Period8 Java/src/guiPractice/Screen.java
+++ Period8 Java/src/guiPractice/Screen.java
@@ -6,7 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
-public class Screen {
+public abstract class Screen {
 	
 	private int width;
 	private int height;
@@ -28,16 +28,16 @@ public class Screen {
 		Graphics2D g = image.createGraphics();
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
-		g.setFont(new Font("Helvetica", Font.PLAIN, 20));
-		g.drawString("Hello", 40, 80);
-		g.drawOval(0, 40, 120, 80);
-		g.drawRect(20, 120, 80, 110);
-		g.drawLine(100, 120, 110, 200);
-		
-		g.setColor(Color.green);
-		for(int i = 0; i <image.getWidth(); i+=2){
-			g.drawLine(i, 230, i, 234);
-		}
+//		g.setFont(new Font("Helvetica", Font.PLAIN, 20));
+//		g.drawString("Hello", 40, 80);
+//		g.drawOval(0, 40, 120, 80);
+//		g.drawRect(20, 120, 80, 110);
+//		g.drawLine(100, 120, 110, 200);
+//		
+//		g.setColor(Color.green);
+//		for(int i = 0; i <image.getWidth(); i+=2){
+//			g.drawLine(i, 230, i, 234);
+//		}
 	}
 	
 	public BufferedImage getImage(){
