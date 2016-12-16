2431accb046a8ab00941602c0952cc4483f70c42
Student8 <Student8@10.8.33.175>
2016-12-16 14:00:30 -0500

    1

--- Period8 Java/src/guiPractice/components/Graphic.java
+++ Period8 Java/src/guiPractice/components/Graphic.java
@@ -1,6 +1,10 @@
 package guiPractice.components;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+
 import guiPractice.components.Visible;
 
 public class Graphic implements Visible {
@@ -17,10 +21,30 @@ public class Graphic implements Visible {
 		this.x = x;
 		this.y = y;
 		loadedImages = false;
-		loadImages(imageLocation, 0.0);
+		loadImages(imageLocation, 0, 0);
 	}
 	
-	private void loadImages(String imageLocation, double d) {
+	private void loadImages(String imageLocation, int w, int h) {
+		try{
+			ImageIcon icon = new ImageIcon(imageLocation);
+			
+			if(w == 0 && h == 0){
+				//use original scale
+				image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+				//draw icon into image
+				Graphics2D g = image.createGraphics();
+				g.drawImage(icon.getImage(), 0, 0 , null);
+			}else{
+				//use custom size (complete on Monday)
+			}
+			loadedImages = true;
+		}catch(Exception e){
+			//in case file is not found
+			e.printStackTrace();
+		}
+	}
+
+	private void loadImages(String imageLocation, double scale) {
 		
 	}
 
