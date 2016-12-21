b436d91076bfaefae81d65f178bd51486ac10f13
Student8 <Student8@10.8.33.175>
2016-12-21 13:39:39 -0500

    1

--- Period8 Java/src/guiPractice/components/ClickableGraphic.java
+++ Period8 Java/src/guiPractice/components/ClickableGraphic.java
@@ -7,17 +7,14 @@ public class ClickableGraphic extends Graphic implements Clickable {
 
 	public ClickableGraphic(int x, int y, double scale, String imageLocation) {
 		super(x, y, scale, imageLocation);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ClickableGraphic(int x, int y, String imageLocation) {
 		super(x, y, imageLocation);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ClickableGraphic(int x, int y, int w, int h, String imageLocation) {
 		super(x, y, w, h, imageLocation);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void setAction(Action a){
